31828ae13954621c7c369490b7de6612a5e7ebeb
GMMarshall <gmmarshall27@gmail.com>
2021-10-20 13:10:47 -0400

    added menu question and switchcase

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const table = require('console.table');
 
+
+
 const db = mysql.createConnection(
     {
       host: 'localhost',
@@ -11,3 +13,49 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the employee_db database.`)
   );
+
+  db.connect(function(err){
+      if (err) throw err;
+      
+  })
+
+  function start() {
+      inquirer .prompt({
+          name: 'menu',
+          type: 'list',
+          message: 'What would you like to do?',
+          choices: [
+              'View all departments',
+              'View all roles',
+              'View all employees',
+              'Add a department',
+              'Add a role',
+              'Add an employee',
+              'Update an employee role'
+          ]
+      }).then(function(answer){
+          switch(answer.menu){
+              case 'View all departments':
+                  viewDeparments();
+                  break;
+                case 'View all roles':
+                    viewRoles();
+                    break;
+                case 'View all employees':
+                    viewEmployees();
+                    break;
+                case 'Add a department':
+                    addDepartment();
+                    break;
+                case 'Add a role':
+                    addRoles();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+                case 'Update an employee role':
+                    updateEmployee();
+                    break;
+          }
+      })
+  }
